73fa7c9c417ac7f85c60327c820fc7153db1a498
vilemhaupt <haupt@volny.cz>
2023-12-22 17:40:44 +0100

    Initial commit

---
+++ src/main/java/me/tauhp/shufflegame/ShuffleGame.java
@@ -0,0 +1,17 @@
+package me.tauhp.shufflegame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShuffleGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
